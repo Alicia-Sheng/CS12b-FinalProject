15556e04940bd36b04b71ae36bcee9c5313a2b61
emmaxyt <63563386+emmaxyt@users.noreply.github.com>
2020-04-28 04:24:07 -0400

    Update TestRun.java
    finalize the readFile method

--- Final Project/src/TestRun.java
+++ Final Project/src/TestRun.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.ListIterator;
 import java.util.Scanner;
 
-public class TestRun2 {
-	//readQuestion
-	public static DoublyLinkedList readFile(String fileName)throws FileNotFoundException {
+public class TestRun {
+	//readQuestions
+	public static DoublyLinkedList readFile(String fileName,String catagory)throws FileNotFoundException {
 		Scanner file=new Scanner(new File(fileName));
 		DoublyLinkedList storeQuestion=new DoublyLinkedList();
-		
+	
 		while (file.hasNextLine()){
 			String onePrompt="";
 			for (int i=0;i<=4;i++) {
 				String line=file.nextLine();
 				onePrompt+=line+"\n";
 			}
-			Question question=new Question(onePrompt);
+			Question question=new Question(onePrompt,catagory);
 			storeQuestion.insertEnd(question);
 		}
 		/**
@@ -27,24 +27,25 @@ public class TestRun2 {
 		**/
 		return storeQuestion;
 	}
-	//readAnswers:haven't re-written yet
-		public static ArrayList<String> readAnswerFile(String fileName)throws FileNotFoundException{
-			Scanner file=new Scanner(new File(fileName));
-			ArrayList<String> answers=new ArrayList<String>();
-			
-			while (file.hasNextLine()) {
-				String line=file.nextLine();
-				answers.add(line);
-			}
-			return answers;
-		}
 
 	public static void main(String[] arg) throws FileNotFoundException{
-		DoublyLinkedList questions=readFile("src/a.txt");
+		DoublyLinkedList biologyQuestion=readFile("src/Biology.txt","Biology");
+		biologyQuestion.readAnswerFile("src/BiologyAnswer.txt");
+		
+		DoublyLinkedList javaQuestion=readFile("src/Java.txt","Java");
+		javaQuestion.readAnswerFile("src/JavaAnswer.txt");
+		
+		DoublyLinkedList movieQuestion=readFile("src/Movie.txt","Movie");
+		movieQuestion.readAnswerFile("src/MovieAnswer.txt");
+		
+		DoublyLinkedList encyQuestion=readFile("src/Java.txt","Encylopedia");
+		encyQuestion.readAnswerFile("src/EncyAnswer.txt");
 		//traverse to test code
-				Node current=questions.head;
-				while (current!=null) {
+				Node current=movieQuestion.head;
+				while (movieQuestion.hasNext(current)) {
 					System.out.println(current.question.printQuestion());
+					System.out.println(current.question.printAnswer());
+					System.out.println(current.question.printCatagory());
 					current=current.next;
 				}
 	}
