a10b44d6548b181b9a461bc8265bdfb7863e7880
Alicia-Sheng <59899141+Alicia-Sheng@users.noreply.github.com>
2020-04-28 14:19:54 +0800

    Modified doubly linked list related classes

--- Final Project/src/DoublyLinkedList.java
+++ Final Project/src/DoublyLinkedList.java
@@ -2,38 +2,76 @@
 public class DoublyLinkedList {
 	public Node head;
 	public Node tail;
-	public int size=0;
-	
+	public int size = 0;
+
 	public DoublyLinkedList() {
-		this.head=null;
-		this.tail=null;
-		this.size=0;
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
 	}
-	public DoublyLinkedList(Node question) {
-		this.head=question;
-		this.tail=question;
-		this.size++;
+
+	public DoublyLinkedList(Question question) {
+		Node node = new Node(question);
+		head = node;
+		tail = node;
+		size = 1;
 	}
-	public Node getHead() {
-		return this.head;
+	
+	public void insertNext(Node node, Question question) {
+		Node newNode = new Node(question);
+		if (node.next == null) {
+			tail.next = node;
+			node.prev = tail;
+			tail = node;
+		}
+		else {
+			newNode.next = node.next;
+			node.next.prev = newNode;
+			node.next = newNode;
+			newNode.prev = node;
+		}
+		size++;
 	}
-	public Node getTail() {
-		return this.tail;
+
+	public boolean hasPrev(Node node) {
+		if (node.prev == null) {
+			return false;
+		}
+		return true;
 	}
+
+	public boolean hasNext(Node node) {
+		if (node.next == null) {
+			return false;
+		}
+		return true;
+	}
+
+//	public DoublyLinkedList(Node question) {
+//		this.head=question;
+//		this.tail=question;
+//		this.size++;
+//	}
+//	public Node getHead() {
+//		return this.head;
+//	}
+//	public Node getTail() {
+//		return this.tail;
+//	}
 	public int getSize() {
 		return this.size;
 	}
-	
-	public void addNode(Question question) {
-		if (this.head==null) {
-			this.head=new Node(question);
-		}else {
-			Node current=this.head;
-			while (current.getNext()!=null) {
-				current=current.getNext();
-			}
-			current.setNext(new Node(question));
-		}
-		
-	}
+
+//	public void addNode(Question question) {
+//		if (this.head==null) {
+//			this.head=new Node(question);
+//		}else {
+//			Node current=this.head;
+//			while (current.getNext()!=null) {
+//				current=current.getNext();
+//			}
+//			current.setNext(new Node(question));
+//		}
+//		
+//	}
 }
--- Final Project/src/Node.java
+++ Final Project/src/Node.java
@@ -1,28 +1,38 @@
 
-public class Node { 
-	private Question question;
-	private Node prev;
-	private Node next;
-	
+public class Node {
+	// private Question question;
+	protected Question question;
+	protected Node prev;
+	protected Node next;
+//	
+//	public Node() {
+//		QA = new String[3]; // QA[0] is Q, QA[1] is A, and QA[2] is category.
+//		//Can also be new String[2] without category.
+//		prev = null;
+//		next = null;
+//	}
+
 	public Node(Question question) {
-		this.question=question;
+		this.question = question;
 	}
-	public Node(Question question,Node prev,Node next) {
-		this.question=question;
-		this.prev=prev;
-		this.next=next;	
-		
-	}
-	public Question getQuestion() {
-		return this.question;
-	}
-	public Node getPrev() {
-		return this.prev;
-	}
-	public Node getNext() {
-		return this.next;
-	}
-	public void setNext(Node next) {
-		this.next=next;
-	}
+
+
+//	public Node(Question question,Node prev,Node next) {
+//		this.question=question;
+//		this.prev=prev;
+//		this.next=next;	
+//		
+//	}
+//	public Question getQuestion() {
+//		return this.question;
+//	}
+//	public Node getPrev() {
+//		return this.prev;
+//	}
+//	public Node getNext() {
+//		return this.next;
+//	}
+//	public void setNext(Node next) {
+//		this.next=next;
+//	}
 }
